92ea9315cb27eebcd423f553e50c9bc2c5ea4f1c
Robert Ramsay <robert.alan.ramsay@gmail.com>
2012-03-26 01:34:00 -0400

    Stats doesn't need a copy constructor

--- prototypes/BattleEngine/BattleEngine/Stats.cs
+++ prototypes/BattleEngine/BattleEngine/Stats.cs
@@ -4,19 +4,13 @@ namespace BattleEngine
 {
 	public struct Stats
 	{
-		public Stats(int speed, int health, int defense, int strength) {
+		public Stats(float speed, int health, int defense, int strength) {
 			this.speed = speed;
 			this.health = health;
 			this.defense = defense;
 			this.strength = strength;
 		}
-		public Stats(Stats stat) {
-			this.speed = stat.speed;
-			this.health = stat.health;
-			this.defense = stat.defense;
-			this.strength = stat.strength;
-		}
-		public int speed;
+		public float speed;
 		public int health;
 		public int defense;
 		public int strength;
