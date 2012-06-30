102a42f3808b0b6f6e5ee3234db31b7d0911595e
Robert Ramsay <robert.alan.ramsay@gmail.com>
2012-06-30 15:46:41 -0400

    Implemented BattleQueue.populate() and DrawableComponent methods.

--- prototypes/BattleEngine/BattleEngine/BattleQueue.cs
+++ prototypes/BattleEngine/BattleEngine/BattleQueue.cs
@@ -29,22 +29,106 @@ namespace BattleEngine
 			public Unit unit;
 			public Unit.Action action;
 			public bool completed;
+			public override int GetHashCode ()
+			{
+				return base.GetHashCode ();
+			}
+			public override bool Equals (object obj)
+			{
+				return base.Equals (obj);
+			}
 		}
+		public static Turn EmptyTurn = new Turn();
+		
+		// Drawable fields
+		static SpriteFont font;
+		
+		SpriteBatch spriteBatch;
+		
 		
 		List<Turn> turnList;
 		int length;
 		int currentIndex;
 		
+		Unit[] party;
+		Unit[] enemies;
+		List<Unit> units;
+		
 		public BattleQueue (Game game, Unit[] party, Unit[] enemies, int length) : base(game)
 		{
 			this.length = length;
 			turnList = new List<Turn>();
+			this.party = party;
+			this.enemies = enemies;
+			units = new List<Unit>();
+		}
+		
+		// Drawable methods
+		protected override void LoadContent ()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+			if (font == null) {
+				font = Game.Content.Load<Microsoft.Xna.Framework.Graphics.SpriteFont>( "Courier New" );
+			}
+			base.LoadContent ();
+		}
+		
+		public override void Update (GameTime gameTime)
+		{
+			base.Update (gameTime);
+			this.populate();
+		}
+		
+		public override void Draw (GameTime gameTime)
+		{
+			Console.Out.Write ("Draw Battle Queue");
+			spriteBatch.Begin ();
+			
+			Vector2 position = Vector2.Zero;
+			Color textColor = Color.White;
+			
+			for (int i = 0; i < length; i++) {
+				if (i >= turnList.Count || turnList[i].unit == null) {
+					spriteBatch.DrawString (
+						font, "Empty", position, 
+	                    textColor);
+				} else {
+					spriteBatch.DrawString (
+						font, turnList[i].unit.Name, position, 
+	                    textColor);
+				}
+				position.Y += 2 + font.MeasureString ("Bob").Y;
+				Console.Out.Write ("    Battle Queue item {0}", i);
+			}
+			spriteBatch.End();
+		}
+		
+		public override void Initialize ()
+		{
+			base.Initialize ();
 		}
 		
 		public void populate()
 		{
-			while (turnList.Count < length) {
-				turnList.Add(new Turn());
+			units.Clear ();
+			units.AddRange (party);
+			units.AddRange(enemies);
+			// Filter out null, dead, etc.
+			foreach( Unit u in units) {
+				if (u == null || u.CurrentStats.health < 1) {
+					units.Remove (u);
+				}
+			}
+			units.Sort(Unit.SpeedComparison);
+			units.Reverse ();
+			
+			Turn turn;
+			while (units.Count > 0 && turnList.Count < length) {
+				foreach (Unit unit in units) {
+					turn = new Turn();
+					turn.unit = unit;
+					turnList.Add(turn);
+				}
 			}
 		}
 		
